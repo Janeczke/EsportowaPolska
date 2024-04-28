9def63be5e4cd2f6250fb72c1d79f94df529a1fb
Szymon <janek00@tuta.io>
2024-04-28 22:36:42 +0200

    Main website ip done

--- main.js
+++ main.js
@@ -18,3 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function copyToClipboard(text) {
+  const el = document.createElement("textarea");
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  alert("Skopiowano do schowka: " + text);
+}
