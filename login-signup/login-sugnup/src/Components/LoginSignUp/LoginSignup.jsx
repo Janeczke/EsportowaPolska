78c4129c313105588705e1759b00ba230a606299
Szymon <janek00@tuta.io>
2024-05-21 01:34:00 +0200

    css worked

--- login-signup/login-sugnup/src/Components/LoginSignUp/LoginSignup.jsx
+++ login-signup/login-sugnup/src/Components/LoginSignUp/LoginSignup.jsx
@@ -1,79 +1,131 @@
-import React, { Component } from 'react'
-import './LoginSignup.scss'
-import { Link, Navigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { Component } from "react";
+import "./LoginSignup.scss";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
 
 class SignIn extends Component {
-
   state = {
-    login: '',
-    email: '',
-    password: '',
-    repeatedPassword: '',
-    errorMsg: '',
-    disableBtn: false
-  }
+    login: "",
+    email: "",
+    password: "",
+    repeatedPassword: "",
+    errorMsg: "",
+    disableBtn: false,
+  };
 
-  handleChange = e => {
-    this.setState({[e.target.id]: e.target.value})
-  }
-  
-  handleSubmit = e => {
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
 
-    if (this.state.username.trim() === '' || this.state.email.trim() === '' || this.state.password === '' || this.state.repeatedPassword === ''){
-      this.setState({errorMsg: 'Brak danych! Uzupełnij formularz.'});
-      this.setState({disableBtn: false});
+    if (
+      this.state.username.trim() === "" ||
+      this.state.email.trim() === "" ||
+      this.state.password === "" ||
+      this.state.repeatedPassword === ""
+    ) {
+      this.setState({ errorMsg: "Brak danych! Uzupełnij formularz." });
+      this.setState({ disableBtn: false });
       return;
     }
 
-    this.setState({errorMsg: 'waiting'})
+    this.setState({ errorMsg: "waiting" });
 
     let config = {
       headers: {
-        "Accept-Language": "pl"
-      }
-    }
+        "Accept-Language": "pl",
+      },
+    };
 
-    axios.post(process.env.REACT_APP_SERVER_URL + '/auth/register', {
-      "username": this.state.username,
-        "email": this.state.email,
-        "password": this.state.password,
-        "repeatedPassword": this.state.repeatedPassword
-      }, config).catch(err => {
-       this.setState({errorMsg: err.response.data.message});
-       this.setState({disableBtn: false});
+    axios
+      .post(
+        process.env.REACT_APP_SERVER_URL + "/auth/register",
+        {
+          username: this.state.username,
+          email: this.state.email,
+          password: this.state.password,
+          repeatedPassword: this.state.repeatedPassword,
+        },
+        config
+      )
+      .catch((err) => {
+        this.setState({ errorMsg: err.response.data.message });
+        this.setState({ disableBtn: false });
       });
-  }
+  };
 
   handleDisableBtn = () => {
-    this.setState({disableBtn: true});
-  }
+    this.setState({ disableBtn: true });
+  };
 
   render() {
-      let errorMsg = <div className="error-msg">{ this.state.errorMsg }</div>
-      if (this.state.errorMsg === 'waiting') {
-        errorMsg = <div className="waiting-msg"></div>
-      }
+    let errorMsg = <div className="error-msg">{this.state.errorMsg}</div>;
+    if (this.state.errorMsg === "waiting") {
+      errorMsg = <div className="waiting-msg"></div>;
+    }
 
-    if (!localStorage.getItem('token')){
-    return (
-      <div className="auth-container">
-      <form className="auth-form" onSubmit={ this.handleSubmit } autoComplete="off">
-        <h1 className="auth-title"> Zarejestruj </h1>
-          <input className="auth-input" maxLength="30" type="text" id="username" placeholder="Nazwa użytkownika" onChange={ this.handleChange } /> 
-          <input className="auth-input" maxLength="30" type="text" id="email" placeholder="Adres email" onChange={ this.handleChange } />
-          <input className="auth-input" maxLength="25" type="password" id="password" placeholder="Hasło" onChange={ this.handleChange } />
-          <input className="auth-input" maxLength="25" type="password" id="repeatedPassword" placeholder="Powtórz hasło" onChange={ this.handleChange } />
-        { errorMsg }
-          <div className="auth-buttons">
-            <input className={this.state.disableBtn ? "auth-btn disable-btn" : "auth-btn"} onClick={ this.handleDisableBtn } type="submit" value="Zarejestruj" />
-            <Link to='/login'><input className="auth-btn" type="button" value="Log in" /></Link>
-          </div>
-      </form>
-      </div>
-    )} else {
-      return <Navigate to='/'/>
+    if (!localStorage.getItem("token")) {
+      return (
+        <div className="auth-container">
+          <form
+            className="auth-form"
+            onSubmit={this.handleSubmit}
+            autoComplete="off"
+          >
+            <h1 className="auth-title"> Zarejestruj </h1>
+            <input
+              className="auth-input"
+              maxLength="30"
+              type="text"
+              id="username"
+              placeholder="Nazwa użytkownika"
+              onChange={this.handleChange}
+            />
+            <input
+              className="auth-input"
+              maxLength="30"
+              type="text"
+              id="email"
+              placeholder="Adres email"
+              onChange={this.handleChange}
+            />
+            <input
+              className="auth-input"
+              maxLength="25"
+              type="password"
+              id="password"
+              placeholder="Hasło"
+              onChange={this.handleChange}
+            />
+            <input
+              className="auth-input"
+              maxLength="25"
+              type="password"
+              id="repeatedPassword"
+              placeholder="Powtórz hasło"
+              onChange={this.handleChange}
+            />
+            {errorMsg}
+            <div className="auth-buttons">
+              <input
+                className={
+                  this.state.disableBtn ? "auth-btn disable-btn" : "auth-btn"
+                }
+                onClick={this.handleDisableBtn}
+                type="submit"
+                value="Zarejestruj"
+              />
+              <Link to="/login">
+                <input className="auth-btn" type="button" value="Zaloguj" />
+              </Link>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return <Navigate to="/" />;
     }
   }
 }
